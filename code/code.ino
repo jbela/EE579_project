8e48d25df06c46d83b2251ff3dfd08f2945deed3
kellywu3 <kellywu533@gmail.com>
2026-04-07 17:06:10 -0700

    Added sinusoidal gait pattern

--- code/code.ino
+++ code/code.ino
@@ -1,110 +1,93 @@
 #include <DynamixelShield.h>
 
 #if defined(ARDUINO_AVR_UNO) || defined(ARDUINO_AVR_MEGA2560)
-  #include <SoftwareSerial.h>
-  SoftwareSerial soft_serial(7, 8); // DYNAMIXELShield UART RX/TX
-  #define DEBUG_SERIAL soft_serial
+    #include <SoftwareSerial.h>
+    SoftwareSerial soft_serial(7, 8); // DYNAMIXELShield UART RX/TX
+    #define DEBUG_SERIAL soft_serial
 #elif defined(ARDUINO_SAM_DUE) || defined(ARDUINO_SAM_ZERO)
-  #define DEBUG_SERIAL SerialUSB    
+    #define DEBUG_SERIAL SerialUSB    
 #else
-  #define DEBUG_SERIAL Serial
+    #define DEBUG_SERIAL Serial
 #endif
 
 const float DXL_PROTOCOL_VERSION = 2.0;
 
-uint8_t IDs[]={0,1,2,3,4,5,6}; // first 2 are back leg, the rest are wheels
-int zeroing_offset[]={150,133,150,60,60}; 
-bool in_dead_zone[]={0,0,0,0,0,0}; //0=not, 1=in // TODO: make sure this is updates for number of motots
+//(max CCW) 1023 <-- 0 (stop) 1024 --> 2047 (max CW)
+const int VELOCITY_CW = 1224;
+const int VELOCITY_CCW = 200;
 
-//for the wheels
-const int CCW_VEL = 500; // (max CCW) 1023 <-- 0 (stop) 1024 --> 2047 (max CW)
-const int CW_VEL = 1500; // (max CW) 1023 <-- 0 (stop) 1024 --> 2047 (max CW)
+uint8_t IDs[] = {1, 2, 3, 4, 5, 6};
+int offset[] = {150, 150, 150, 60, 60, 60};
+float phase_difference[] = {0, 0, 0, 0, 0, 0};
+bool direction[] = {1, 0, 1, 0, 1, 0};
 
-//for the legs
-unsigned long start = 0.0;
-const unsigned long period = 2000; //period of gait
+const int gait_cycle_ms = 1000;
+const float oscillation_range = 90.0;
 
+float get_angle(int i, int gait_cycle_time) {
+    float cycle_percentage = phase_difference[i] + gait_cycle_time / (float) gait_cycle_ms;
+
+    if(cycle_percentage > 1) {
+        cycle_percentage -= 1;
+    }
+
+    float angle = (oscillation_range / 2.0) * sin(2.0 * PI * cycle_percentage);
+
+    if(direction[i] == 1) {
+        angle = - angle + offset[i];
+    } else {
+        angle = angle + offset[i];
+    }
+
+    if(angle >= 360.0) {
+        angle -= 360.0;
+    } else if(angle < 0.0) {
+        angle += 360;
+    }
+
+    return angle;
+}
+
+unsigned long start = 0;
 DynamixelShield dxl;
 
-//This namespace is required to use Control table item names
-using namespace ControlTableItem;
-
 void setup() {
-  // For Uno, Nano, Mini, and Mega, use UART port of DYNAMIXEL Shield to debug.
-  DEBUG_SERIAL.begin(115200);
+    DEBUG_SERIAL.begin(115200);
 
-  dxl.begin(1000000);
-  // Set Port Protocol Version. This has to match with DYNAMIXEL protocol version.
-  dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
+    dxl.begin(1000000);
+    dxl.setPortProtocolVersion(DXL_PROTOCOL_VERSION);
 
-  for(int i=0;i < sizeof(IDs); i++){
-    dxl.ping(IDs[i]);
-    dxl.torqueOff(IDs[i]);
-    if(i < 2) {
-      dxl.setOperatingMode(IDs[i], OP_POSITION);
+    for(int i = 0; i < sizeof(IDs); i++){
+        dxl.ping(IDs[i]);
+        dxl.torqueOff(IDs[i]);
+        if(i < 2) {
+            dxl.setOperatingMode(IDs[i], OP_POSITION);
+        } else {
+            dxl.setOperatingMode(IDs[i], OP_VELOCITY);
+        }
+        dxl.torqueOn(IDs[i]);
     }
-    else {
-      dxl.setOperatingMode(IDs[i], OP_VELOCITY);
-    }
-    dxl.torqueOn(IDs[i]);
-  }
-  start = millis();
+    start = millis();
 }
 
-
-
-gait_phase(int time, int leg, int mode=0) {
-  //we will set the neutral stright back position as the zero position
-  // TODO: need to edit this to accomodate several more gaits. 
-  float position = 30* sin(time * 2* PI);
-  dxl.setGoalPosition(IDs[leg], position, UNIT_DEGREE);
-
-}
-
-void leg_gait() { // TODO: update this gait function to support our project's gait.
-  unsigned long elapsed = millis() - start;
-  float time = fmod((float)elapsed, period) / (float)period; // from 0 to 1
-
-  // first leg
-  gait_phase(time, 0);
-
-  float time2 = time + 0.5; // phase shift :TODO
-  if (time2 > 1.0) {
-    time2 -= 1.0;
-  }
-
-  gait_phase(time2, 1);  //other leg
-}
-
+int prev_time = 0;
 
 void loop() {
+    unsigned long elapsed = millis() - start;
 
-  unsigned long elapsed = millis() - start;
-  float time = fmod((float)elapsed, period) / (float)period;
-  
-  //LEG_GAIT
-  // leg_gait();
- 
-  //WHEEL GAIT
-  //TDOD: clean this up once we figure out which wheel is which. 
-  dxl.setGoalVelocity(IDs[2], CW_VEL); //(max CCW) 1023 <-- 0 (stop) 1024 --> 2047 (max CW)
-  dxl.setGoalVelocity(IDs[3], CCW_VEL); //(max CCW) 1023 <-- 0 (stop) 1024 --> 2047 (max CW)
-  dxl.setGoalVelocity(IDs[4], CW_VEL); //(max CCW) 1023 <-- 0 (stop) 1024 --> 2047 (max CW)  dxl.setGoalVelocity(IDs[4], CCW_VEL); //(max CCW) 1023 <-- 0 (stop) 1024 --> 2047 (max CW)
-  dxl.setGoalVelocity(IDs[5], CCW_VEL); //(max CCW) 1023 <-- 0 (stop) 1024 --> 2047 (max CW)  dxl.setGoalVelocity(IDs[5], CW_VEL); //(max CCW) 1023 <-- 0 (stop) 1024 --> 2047 (max CW)
+    int gait_cycle_time = elapsed - prev_time;
+    if(gait_cycle_time > gait_cycle_ms) {
+        prev_time = elapsed;
+        gait_cycle_time = 0;
+    }
+
+    for(int i = 0; i < 2; i++) {
+        float angle = get_angle(i, gait_cycle_time);
+        dxl.setGoalAngle(IDs[i], angle);
+    }
+
+    for(int i = 2; i < 6; i++) {
+        int velocity = direction[i] ? VELOCITY_CW : VELOCITY_CCW;
+        dxl.setGoalVelocity(IDs[i], velocity);
+    }
 }
-
-/*
-- how to measure "speed"
-- what is considered "better performance"
-- what params are we varying
-- 
-
-
-1: 60, 
-2: 60,
-3: -, 
-4: -, 
-5: -, 
-6: -,
-
-*/
